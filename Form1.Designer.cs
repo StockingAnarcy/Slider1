d14b8d4d617d29f0040d2e7cbe952f62594481e0
stockinanarcy <stockinanarcy@gmail.com>
2022-07-21 03:09:08 +0300

    Upd

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,21 +30,13 @@ namespace Slider1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1940, 1080);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // timer1
             // 
@@ -52,11 +44,50 @@ namespace Slider1
             this.timer1.Interval = 3000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1366, 768);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(124, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(124, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1940, 1080);
+            this.ClientSize = new System.Drawing.Size(1386, 788);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
@@ -64,13 +95,16 @@ namespace Slider1
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace Slider1
 {
     public partial class Form1 : Form
     {
+        string imagePath = @"./Images/";
+        string filePath = @"./";
+        string fileName = "demo.txt";
+
+        List<string> lines = new List<string>();
+        List<Label> labels = new List<Label>();
+        
+
         public Form1()
         {
+            
+            
+
             this.WindowState = FormWindowState.Maximized;
+           
+            var watcher = new FileSystemWatcher(filePath);
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += OnChanged;
+            watcher.Filter = "*.txt";
+            
             InitializeComponent();
         }
         private int ImageNumber = 1;
@@ -26,10 +44,10 @@ namespace Slider1
             {
                 ImageNumber = 1;
             }
-            pictureBox1.ImageLocation = string.Format(@"C:\Project\Slider1\Images\"+ImageNumber+".jpg");
+            pictureBox1.ImageLocation = string.Format(imagePath + ImageNumber+".jpg");
         }
 
-        private void LoadPreviousImages()
+       /* private void LoadPreviousImages()
         {
             timer1.Start();
             ImageNumber--;
@@ -37,17 +55,92 @@ namespace Slider1
             {
                 ImageNumber = 5;
             }
-            pictureBox1.ImageLocation = string.Format(@"C:\Project\Slider1\Images\" + ImageNumber + ".jpg");
+            pictureBox1.ImageLocation = string.Format(imagePath + ImageNumber + ".jpg");
+        }*/
+
+        public void CheckFile()
+        {
+            timer2.Start();
+            if (File.Exists(filePath + @"\" + fileName)) //если файл сущесвует
+            {
+                //string ln;
+                labels = this.Controls.OfType<Label>().ToList();
+                StreamReader sr = new StreamReader(fileName);
+                
+                while (sr.Peek() >= 0)
+                {
+                    lines.Add(sr.ReadLine());
+                } 
+                sr.Close(); 
+                    
+                for (int i = 0; i < labels.Count; i++) //вывод в текст
+                {
+                    labels[i].Text = lines[i].Substring(lines[i].IndexOf(':') + 1); 
+                }
+
+            }
+            else    //если файл отсутствует
+            {
+                lines.Clear();
+                for(int i = 0; i < labels.Count; i++) 
+                {
+
+                    labels[i].Text = "";
+
+                }
+            }
+        }
+
+        //private void ReadFile()
+        //{
+        //    labels = this.Controls.OfType<Label>().ToList();
+        //    StreamReader sr = new StreamReader(fileName);
+        //    while (!sr.EndOfStream) //читаем файл
+        //    {
+
+        //        lines.Add(sr.ReadLine());
+
+        //    }
+        //    sr.Close();
+
+        //    for (int i = 0; i < labels.Count; i++) //вывод в текст
+        //    {
+
+        //        labels[i].Text = lines[i].Substring(lines[i].IndexOf(':') + 1);
+
+        //    }
+        //}
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+              return;
+            }
+            CheckFile();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadNextImages();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            CheckFile();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void timer1_Tick(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            LoadNextImages();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
